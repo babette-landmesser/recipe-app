976374b29a9ef770ad2df55a35fee6132f958f63
Babette Landmesser <babette.landmesser@cocomore.com>
2018-03-06 08:40:54 +0100

    WIP: style login page

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Login} from '../../core/models/login';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  model: Login;
+  loginFormHasErrors: boolean;
 
-  constructor() { }
+  constructor() {
+    this.model = new Login();
+  }
 
   ngOnInit() {
   }
